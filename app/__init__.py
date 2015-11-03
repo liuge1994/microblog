e5511b82b785b6b5df5d42f2239d0342eca7cb7e
liuge1994 <610562301@qq.com>
2015-11-03 12:41:03 +0800

    add the loginmanager

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,17 @@
+import os
 from flask import Flask
+from flask.ext.login import LoginManager
+from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.openid import OpenID
+from config import basedir
+
+lm = LoginManager()
+lm.init_app(app)
+oid = OpenID(app, os.path.join(basedir, 'tmp'))
 
 app = Flask(__name__)
 app.config.from_object('config')
+db = SQLAlchemy(app)
 
-from app import views
+from app import views, models
 
